2db14224d3989954aee456fc66f63ee23b1ba886
anandgitacc <anand.akmail@gmail.com>
2022-02-17 10:08:25 +0530

    firstgit

---
+++ src/org/gittest/GitOne.java
@@ -0,0 +1,26 @@
+package org.gittest;
+
+public class GitOne {
+	
+	private void one() {
+		System.out.println("git test 1");
+	}
+	
+	private void two() {
+		System.out.println("git test 2");
+	}
+	
+	private void three() {
+		System.out.println("git test 3");
+	}
+	
+	public static void main(String[] args) {
+		
+		GitOne g = new GitOne();
+		g.one();
+		g.two();
+		g.three();
+		
+	}
+
+}
